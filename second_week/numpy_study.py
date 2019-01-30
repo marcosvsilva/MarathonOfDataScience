27d2c5c8fee6817fcf3624a42a54e5afc65584a2
Marcos Vinicius <marcos.v.silva@live.com>
2019-01-29 23:52:56 -0200

    study continue

--- second_week/numpy_study.py
+++ second_week/numpy_study.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 # First 20 countries with school completion data
-countries = np.array([
+countries_completion = np.array([
     'Algeria', 'Argentina', 'Armenia', 'Aruba', 'Austria', 'Azerbaijan',
     'Bahamas', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Bolivia',
     'Botswana', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi',
     'Cambodia', 'Cameroon', 'Cape Verde'
+])
+
+# First 20 countries with employment data
+countries_employment = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
 ])
 
 # Female school completion rate in 2007 for those 20 countries
@@ -24,11 +33,28 @@ male_completion = np.array([
     37.00692, 45.39401, 91.22084, 62.42028, 90.66958
 ])
 
+employment = np.array([
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+])
+
 
 def overall_completion_rate(female, male):
     return (female + male) / 2
 
+def standardize_data(values):
+    standardize_values = (values - values.mean()) / values.std()
+    return standardize_values
+
 
 sum_completion = overall_completion_rate(female_completion, male_completion)
-for i in range(len(sum_completion)):
-    print('Country {} sum_completion {}'.format(countries[i], sum_completion[i]))
+for i in range(len(countries_completion)):
+    print('Country {} sum_completion {}'.format(countries_completion[i], sum_completion[i]))
+
+print('\n')
+standardize = standardize_data(employment)
+for i in range(len(countries_employment)):
+    print('Country {} sum_completion {}'.format(countries_employment[i], standardize[i]))
