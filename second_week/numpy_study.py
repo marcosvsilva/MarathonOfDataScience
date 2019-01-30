380a60d1009190ad2e57015ebee441f1d8f59460
Marcos Vinicius <marcos.v.silva@live.com>
2019-01-30 00:14:17 -0200

    study continue

--- second_week/numpy_study.py
+++ second_week/numpy_study.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-# First 20 countries with school completion data
+'''
+1- study countrys and genere completition
+'''
+
+
+def overall_completion_rate(female, male):
+    return (female + male) / 2
+
+
 countries_completion = np.array([
     'Algeria', 'Argentina', 'Armenia', 'Aruba', 'Austria', 'Azerbaijan',
     'Bahamas', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Bolivia',
@@ -8,16 +16,6 @@ countries_completion = np.array([
     'Cambodia', 'Cameroon', 'Cape Verde'
 ])
 
-# First 20 countries with employment data
-countries_employment = np.array([
-    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
-    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
-    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
-    'Belize', 'Benin', 'Bhutan', 'Bolivia',
-    'Bosnia and Herzegovina'
-])
-
-# Female school completion rate in 2007 for those 20 countries
 female_completion = np.array([
     97.35583, 104.62379, 103.02998, 95.14321, 103.69019,
     98.49185, 100.88828, 95.43974, 92.11484, 91.54804,
@@ -25,12 +23,36 @@ female_completion = np.array([
     29.07386, 38.41644, 90.70509, 51.7478, 95.45072
 ])
 
-# Male school completion rate in 2007 for those 20 countries
 male_completion = np.array([
     95.47622, 100.66476, 99.7926, 91.48936, 103.22096,
     97.80458, 103.81398, 88.11736, 93.55611, 87.76347,
     102.45714, 98.73953, 92.22388, 115.3892, 98.70502,
     37.00692, 45.39401, 91.22084, 62.42028, 90.66958
+])
+
+
+sum_completion = overall_completion_rate(female_completion, male_completion)
+for i in range(len(countries_completion)):
+    print('Country {} sum_completion {}'.format(countries_completion[i], sum_completion[i]))
+print('\n')
+
+
+'''
+2- study normalization
+'''
+
+
+def standardize_data(values):
+    standardize_values = (values - values.mean()) / values.std()
+    return standardize_values
+
+
+countries_employment = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
 ])
 
 employment = np.array([
@@ -41,20 +63,36 @@ employment = np.array([
     71.59999847,  58.40000153,  70.40000153,  41.20000076
 ])
 
-
-def overall_completion_rate(female, male):
-    return (female + male) / 2
-
-def standardize_data(values):
-    standardize_values = (values - values.mean()) / values.std()
-    return standardize_values
-
-
-sum_completion = overall_completion_rate(female_completion, male_completion)
-for i in range(len(countries_completion)):
-    print('Country {} sum_completion {}'.format(countries_completion[i], sum_completion[i]))
-
-print('\n')
 standardize = standardize_data(employment)
 for i in range(len(countries_employment)):
-    print('Country {} sum_completion {}'.format(countries_employment[i], standardize[i]))
+    print('Country {} standardize {}'.format(countries_employment[i], standardize[i]))
+print('\n')
+
+
+'''
+3- study index array 
+'''
+
+
+def mean_time_for_paid_students(time_spent, days_to_cancel):
+    index = (days_to_cancel >= 7)
+    print(index)
+    print(time_spent[index])
+    return np.mean(time_spent[index])
+
+
+time_spent = np.array([
+       12.89697233,    0.        ,   64.55043217,    0.        ,
+       24.2315615 ,   39.991625  ,    0.        ,    0.        ,
+      147.20683783,    0.        ,    0.        ,    0.        ,
+       45.18261617,  157.60454283,  133.2434615 ,   52.85000767,
+        0.        ,   54.9204785 ,   26.78142417,    0.
+])
+
+days_to_cancel = np.array([
+      4,   5,  37,   3,  12,   4,  35,  38,   5,  37,   3,   3,  68,
+     38,  98,   2, 249,   2, 127,  35
+])
+
+standardize = standardize_data(employment)
+print('Mean {}'.format(mean_time_for_paid_students(time_spent, days_to_cancel)))
