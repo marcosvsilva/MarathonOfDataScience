69e139cf048785bef3b9259a90ec3f2e5c441995
Marcos Vinicius <marcos.v.silva@live.com>
2019-02-20 23:41:20 -0300

    Study pandas group by

---
+++ second_week/pandas_groupby.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib
+matplotlib.use("wxAgg")
+import matplotlib.pyplot as plt
+
+filename = '../dataset/nyc_subway_weather.csv'
+subway_df = pd.read_csv(filename)
+
+rideship_by_day = subway_df.groupby('day_week').mean()['ENTRIESn_hourly']
+
+print(rideship_by_day)
+plt.plot(rideship_by_day)
+plt.show()
